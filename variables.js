90b5b142ccf45583b5288ad352835fec0735ed45
Bhoomika07 <bhoomikab.xworkz>
2025-11-14 12:44:36 +0530

    variables in JS

---
+++ variables.js
@@ -0,0 +1,14 @@
+//var speed = 5;//declaration
+//var speed = false;//re declaration
+//speed ="120KM/ph";
+ 
+//console.log(speed);
+
+//let price=10;
+//console.log(price);
+//in this(temporal and dead zone)
+//let price = 10;
+//we can't re declare it but we can re assign 
+//console.log(total);
+const total = 100;
+console.log(total);
