4c6cf4d344f1a7e058123ca4ad66605836c8e3dc
Bhoomika07 <bhoomikab.xworkz>
2025-11-14 12:39:06 +0530

    javascript func n blocks

---
+++ func and blocks.js
@@ -0,0 +1,15 @@
+//var name = "Bhoomi";
+let b = 10;
+
+function a(){
+    if(true){
+        //let name ="Bhoomika";
+        //console.log(name);
+        let b = "string";
+
+    }
+   // console.log(name);
+   console.log(b);
+
+}
+a();
