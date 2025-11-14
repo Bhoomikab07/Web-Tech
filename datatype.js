413d4548ecca3615ac10294a53345b279ed46ad3
Bhoomika07 <bhoomikab.xworkz>
2025-11-14 12:43:25 +0530

    datatypes in JS

---
+++ datatype.js
@@ -0,0 +1,34 @@
+console.log("datatype....");
+var a = 10000000000000;
+console.log(typeof a);
+
+var b = 1n;
+console.log(typeof b);
+
+var c = true;
+console.log(typeof c);
+
+var d = "Bhoomi";
+console.log(typeof d);
+
+var e = Symbol("B");
+console.log(typeof e);
+
+var f = undefined;
+console.log(typeof f);
+
+var g = [0,1,2,3,false,"car"];
+console.log(typeof g[3]);
+
+var h = {name:"Bhoomi", age:23};
+console.log(h.name);
+console.log(typeof h);
+
+var i = null;
+console.log(typeof i);
+
+
+
+
+
+
